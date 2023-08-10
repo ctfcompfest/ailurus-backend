1cf6b9a97bd32cbcb7e5340675a4e15320ebec20
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2023-08-10 10:34:53 +0700

    change access atribute from sqlalchemy to flask_sqlalchemy object

--- and_platform/models/__init__.py
+++ and_platform/models/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 import enum
 import secrets
-import sqlalchemy as sa
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -11,110 +10,110 @@ migrate = Migrate()
 class Configs(db.Model):
     __tablename__ = "configs"
     
-    id = sa.Column(sa.Integer, primary_key=True)
-    key = sa.Column(sa.String(128), unique=True, index=True)
-    value = sa.Column(sa.Text)
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String(128), unique=True, index=True)
+    value = db.Column(db.Text)
 
 class Servers(db.Model):
     __tablename__ = "servers"
     
-    id = sa.Column(sa.Integer, primary_key=True)
-    host = sa.Column(sa.String(50), unique=True)
-    sshport = sa.Column(sa.Integer)
-    username = sa.Column(sa.String(50))
-    auth_key = sa.Column(sa.Text)
+    id = db.Column(db.Integer, primary_key=True)
+    host = db.Column(db.String(50), unique=True)
+    sshport = db.Column(db.Integer)
+    username = db.Column(db.String(50))
+    auth_key = db.Column(db.Text)
 
 class Teams(db.Model):
     __tablename__ = "teams"
 
-    id = sa.Column(sa.Integer, primary_key=True)
-    name = sa.Column(sa.String(128))
-    email = sa.Column(sa.String(128), unique=True, index=True)
-    password = sa.Column(sa.String)
-    server_id = sa.Column(sa.Integer, sa.ForeignKey("servers.id"), unique=True)
-    server_host = sa.Column(sa.String)
-    secret = sa.Column(sa.String(50), default=secrets.token_urlsafe(32))
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    email = db.Column(db.String(128), unique=True, index=True)
+    password = db.Column(db.String)
+    server_id = db.Column(db.Integer, db.ForeignKey("servers.id"), unique=True)
+    server_host = db.Column(db.String)
+    secret = db.Column(db.String(50), default=secrets.token_urlsafe(32))
 
 class Challenges(db.Model):
     __tablename__ = "challenges"
 
-    id = sa.Column(sa.Integer, primary_key=True)
-    name = sa.Column(sa.String(80))
-    description = sa.Column(sa.Text)
-    num_expose = sa.Column(sa.Integer, default=1)
-    server_id = sa.Column(sa.Integer, sa.ForeignKey("servers.id"), unique=True)
-    server_host = sa.Column(sa.String)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    description = db.Column(db.Text)
+    num_expose = db.Column(db.Integer, default=1)
+    server_id = db.Column(db.Integer, db.ForeignKey("servers.id"), unique=True)
+    server_host = db.Column(db.String)
 
 class Flags(db.Model):
     __tablename__ = "flags"
     __table_args__ = (
-        sa.UniqueConstraint("team_id", "challenge_id", "round", "tick"),
+        db.UniqueConstraint("team_id", "challenge_id", "round", "tick"),
     )
 
-    id = sa.Column(sa.Integer, primary_key=True)
-    team_id = sa.Column(sa.Integer, sa.ForeignKey("teams.id"))
-    challenge_id = sa.Column(sa.Integer, sa.ForeignKey("challenges.id"))
-    round = sa.Column(sa.Integer)
-    tick = sa.Column(sa.Integer)
-    value = sa.Column(sa.Text, index=True)
+    id = db.Column(db.Integer, primary_key=True)
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id"))
+    challenge_id = db.Column(db.Integer, db.ForeignKey("challenges.id"))
+    round = db.Column(db.Integer)
+    tick = db.Column(db.Integer)
+    value = db.Column(db.Text, index=True)
     
 class Submissions(db.Model):
     __tablename__ = "submissions"
 
-    id = sa.Column(sa.Integer, primary_key=True)
-    team_id = sa.Column(sa.Integer, sa.ForeignKey("teams.id"))
-    challenge_id = sa.Column(sa.Integer, sa.ForeignKey("challenges.id"))
-    round = sa.Column(sa.Integer)
-    tick = sa.Column(sa.Integer)
-    value = sa.Column(sa.Text, index=True)
-    verdict = sa.Column(sa.Boolean)
+    id = db.Column(db.Integer, primary_key=True)
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id"))
+    challenge_id = db.Column(db.Integer, db.ForeignKey("challenges.id"))
+    round = db.Column(db.Integer)
+    tick = db.Column(db.Integer)
+    value = db.Column(db.Text, index=True)
+    verdict = db.Column(db.Boolean)
 
 class Solves(db.Model):
     __tablename__ = "solves"
     __table_args__ = (
-        sa.PrimaryKeyConstraint("team_id", "challenge_id"),
+        db.PrimaryKeyConstraint("team_id", "challenge_id"),
     )
 
-    team_id = sa.Column(sa.Integer, sa.ForeignKey("teams.id"))
-    challenge_id = sa.Column(sa.Integer, sa.ForeignKey("challenges.id"))
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id"))
+    challenge_id = db.Column(db.Integer, db.ForeignKey("challenges.id"))
 
 class ChallengeReleases(db.Model):
     __tablename__ = "challenge_releases"
     __table_args__ = (
-        sa.PrimaryKeyConstraint("round", "challenge_id"),
+        db.PrimaryKeyConstraint("round", "challenge_id"),
     )
 
-    round = sa.Column(sa.Integer)
-    challenge_id = sa.Column(sa.Integer, sa.ForeignKey("challenges.id"))
+    round = db.Column(db.Integer)
+    challenge_id = db.Column(db.Integer, db.ForeignKey("challenges.id"))
 
 class ScorePerTicks(db.Model):
     __tablename__ = "score_per_ticks"
     __table_args__ = (
-        sa.UniqueConstraint("round", "tick", "team_id"),
+        db.UniqueConstraint("round", "tick", "team_id"),
     )
 
-    id = sa.Column(sa.Integer, primary_key=True)
-    round = sa.Column(sa.Integer)
-    tick = sa.Column(sa.Integer)
-    team_id = sa.Column(sa.Integer, sa.ForeignKey("teams.id"))
-    attack_score = sa.Column(sa.Double)
-    defense_score = sa.Column(sa.Double)
-    sla = sa.Column(sa.Double)
+    id = db.Column(db.Integer, primary_key=True)
+    round = db.Column(db.Integer)
+    tick = db.Column(db.Integer)
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id"))
+    attack_score = db.Column(db.Double)
+    defense_score = db.Column(db.Double)
+    sla = db.Column(db.Double)
 
-    team = sa.relationship("Teams", foreign_keys="ScorePerTicks.team_id", lazy="joined")
+    team = db.relationship("Teams", foreign_keys="ScorePerTicks.team_id", lazy="joined")
 
 class Services(db.Model):
     __tablename__ = "services"
     __table_args__ = (
-        sa.UniqueConstraint("team_id", "challenge_id", "order"),
-        sa.Index("service_idx", "team_id", "challenge_id"),
+        db.UniqueConstraint("team_id", "challenge_id", "order"),
+        db.Index("service_idx", "team_id", "challenge_id"),
     )
 
-    id = sa.Column(sa.Integer, primary_key=True)
-    team_id = sa.Column(sa.Integer, sa.ForeignKey("teams.id"))
-    challenge_id = sa.Column(sa.Integer, sa.ForeignKey("challenges.id"))
-    order = sa.Column(sa.Integer)
-    address = sa.Column(sa.String(100), unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id"))
+    challenge_id = db.Column(db.Integer, db.ForeignKey("challenges.id"))
+    order = db.Column(db.Integer)
+    address = db.Column(db.String(100), unique=True)
 
 class CheckerVerdict(enum.Enum):
     QUEUE = -1
@@ -125,7 +124,7 @@ class CheckerVerdict(enum.Enum):
 class CheckerQueues(db.Model):
     __tablename__ = "checker_queues"
 
-    id = sa.Column(sa.Integer, primary_key=True)
-    team_id = sa.Column(sa.Integer, sa.ForeignKey("teams.id"))
-    challenge_id = sa.Column(sa.Integer, sa.ForeignKey("challenges.id"))
-    result = sa.Column(sa.Enum(CheckerVerdict))
+    id = db.Column(db.Integer, primary_key=True)
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id"))
+    challenge_id = db.Column(db.Integer, db.ForeignKey("challenges.id"))
+    result = db.Column(db.Enum(CheckerVerdict))
