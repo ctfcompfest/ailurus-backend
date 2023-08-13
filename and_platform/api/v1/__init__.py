40a30c1a2617f2fbc604acb75fbc6fa406746d2f
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2023-08-13 19:14:37 +0700

    implement flag submission api (#21)

--- and_platform/api/v1/__init__.py
+++ and_platform/api/v1/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 from and_platform.api.v1.admin import adminapi_blueprint
+from and_platform.api.v1.submission import submission_blueprint
 
 apiv1_blueprint = Blueprint("apiv1", __name__, url_prefix="/v1")
 apiv1_blueprint.register_blueprint(adminapi_blueprint)
+apiv1_blueprint.register_blueprint(submission_blueprint)
--- and_platform/core/security.py
+++ and_platform/core/security.py
@@ -1,9 +1,29 @@
-from flask import jsonify, request
 from and_platform.core.config import get_config
+from and_platform.models import db, Teams
+from flask import current_app as app, jsonify, request
+
+import jwt
 
 def admin_only():
     req_secret = request.headers.get("x-adce-secret", None)
 
     # If server admin forgot to set ADCE_SECRET, all request to the admin API are forbid
     if get_config("ADCE_SECRET") == None or req_secret != get_config("ADCE_SECRET"):
+        return jsonify(status="forbidden", message="forbidden."), 403
+
+def validteam_only():
+    SECRET = app.config["SECRET_KEY"]
+    req_auth = request.headers.get("authorization")
+    if req_auth == None:
+        return jsonify(status="forbidden", message="forbidden."), 403
+    
+    req_jwt = req_auth.split(" ")[-1]
+    try:
+        jwt_claim = jwt.decode(req_jwt, SECRET, algorithms=["HS512"])
+        team = db.session.execute(
+            Teams.__table__.select().where(Teams.id == jwt_claim["team"]["id"])
+        ).fetchone()
+        if team == None:
+            return jsonify(status="forbidden", message="forbidden."), 403
+    except:
         return jsonify(status="forbidden", message="forbidden."), 403
