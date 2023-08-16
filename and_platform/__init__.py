728fb42aefd9a978f5c489944c88beaa13d427af
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2023-08-16 11:14:23 +0700

    Fix bug when running flask db upgrade for the first time (#23)
    * add exception to detect flask-migrate
    
    * move exception catch to create_app function
    
    * add warning message

--- and_platform/__init__.py
+++ and_platform/__init__.py
@@ -14,6 +14,7 @@ from and_platform.core.config import get_config, set_config
 from typing import List
 
 import os
+import sqlalchemy
 
 def manage(args: List[str]):
     if len(args) < 1:
@@ -30,6 +31,7 @@ def load_adce_config():
         realkey = key[5:]
         if not key.startswith("ADCE_") or get_config(realkey) != None: continue
         set_config(realkey, value)
+    
 
 def create_app():
     app = Flask(
@@ -45,7 +47,11 @@ def create_app():
         db.init_app(app)
         migrate.init_app(app, db)
         
-        load_adce_config()
+        try:
+            load_adce_config()
+        except sqlalchemy.exc.ProgrammingError:
+            # To detect that the relation has not been created yet
+            app.logger.warning("Error calling some function while create_app")
 
         # Blueprints
         app.register_blueprint(api_blueprint)
