7ca18d0d3db6cd2c63689a3c5995d08c7797c309
Rhfialyndra <l>
2023-08-17 22:17:39 +0700

    implement recursion

--- and_platform/api/helper/__init__.py
+++ and_platform/api/helper/__init__.py
@@ -2,9 +2,7 @@ def convert_model_to_dict(model):
     if isinstance(model, list):
         model_list = []
         for item in model:
-            model_data = item.__dict__.copy()
-            model_data.pop('_sa_instance_state', None)
-            model_list.append(model_data)
+            model_list.append(convert_model_to_dict(item))
         return model_list
     else:
         model_data = model.__dict__.copy()
