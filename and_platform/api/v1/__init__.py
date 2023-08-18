2fd6eb7548c1dda294ec5a290b5fb913af5a9e6d
not4saken <87106378+Rhfialyndra@users.noreply.github.com>
2023-08-19 00:02:36 +0700

    implements contest/info (#29)
    Co-authored-by: Rhfialyndra <l>

--- and_platform/api/v1/__init__.py
+++ and_platform/api/v1/__init__.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
 from and_platform.api.v1.admin import adminapi_blueprint
 from and_platform.api.v1.submission import submission_blueprint
+from and_platform.api.v1.contest import public_contest_blueprint
 
 apiv1_blueprint = Blueprint("apiv1", __name__, url_prefix="/v1")
 apiv1_blueprint.register_blueprint(adminapi_blueprint)
 apiv1_blueprint.register_blueprint(submission_blueprint)
+apiv1_blueprint.register_blueprint(public_contest_blueprint)
