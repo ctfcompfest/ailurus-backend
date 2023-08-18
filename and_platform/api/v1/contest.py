15ea56ee42c8ad13fae4be7fdc6f40be82604c5c
Rhfialyndra <l>
2023-08-18 08:24:03 +0700

    implements contest/info

---
+++ and_platform/api/v1/contest.py
@@ -0,0 +1,35 @@
+from and_platform.core.config import get_config
+from flask import Blueprint, jsonify;
+
+public_contest_blueprint = Blueprint("public_contest_blueprint", __name__, url_prefix="/contest")
+
+@public_contest_blueprint.route("/info", methods=["GET"])
+def get_contest_config():
+    event_name = get_config("EVENT_NAME", "AnD")
+    start_time = get_config("START_TIME", "2007-03-01T13:00:00Z")
+    number_round = get_config("NUMBER_ROUND", 0)
+    number_tick = get_config("NUMBER_TICK", 0)
+    tick_duration = get_config("TICK_DURATION", 0)
+    event_status = {}
+    current_round = get_config("CURRENT_ROUND", 0)
+    current_tick = get_config("CURRENT_TICK", 0)
+
+    if current_round == 0:
+        event_status["state"] = "not started"
+    elif current_round > number_round:
+        event_status["state"] = "finished"
+    else:
+        event_status["state"] = "running"
+        event_status["current_round"] = current_round
+        event_status["current_tick"] = current_tick
+    
+    info = {
+        "event_name" : event_name,
+        "event_status" : event_status,
+        "start_time" : start_time,
+        "number_round" : number_round,
+        "number_tick" : number_tick,
+        "tick_duration" : tick_duration
+    }
+
+    return jsonify(status="success", data=info), 200
