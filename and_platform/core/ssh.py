1af94414a26a489ed03f0d84bc072cc5c58a3d29
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2023-08-18 23:09:35 +0700

    fix private key type detection

--- and_platform/core/ssh.py
+++ and_platform/core/ssh.py
@@ -1,4 +1,5 @@
 from and_platform.models import Servers
+from base64 import b64decode
 from fabric import Connection, Result
 from io import StringIO
 from os import PathLike
@@ -8,6 +9,7 @@ from typing import List
 import os
 import paramiko
 import tarfile
+import re
 
 def execute_remote_command(host: str, *cmds: List[str]) -> List[Result]:
     conn = Connection(host)
@@ -25,18 +27,21 @@ def copy_file(host: str, path: PathLike, remote: PathLike):
     return conn.put(local_path, remote=remote_path)
 
 def create_ssh_from_server(server: Servers) -> Connection:
-    key_mode = server.auth_key[:server.auth_key.find("PRIVATE")-1]
-    key_mode = key_mode[key_mode.find(" ")+1:]
+    sshkey = "".join(server.auth_key.split("\n")[1:-1])
+    # Find "ssh-<something>" except "ssh-key"
+    key_mode = re.search(b"ssh-(?!key)[A-Za-z0-9]+", b64decode(sshkey.encode())).group(0).decode()
     
-    keyfile = StringIO(server.auth_key.replace(key_mode, "OPENSSH"))
-    if key_mode == "ED25519":
+    keyfile = StringIO(server.auth_key)
+    if key_mode == "ssh-ed25519":
         pkey = paramiko.Ed25519Key.from_private_key(keyfile)
-    elif key_mode == "ECDSA":
+    elif key_mode == "ssh-ecdsa":
         pkey = paramiko.ECDSAKey.from_private_key(keyfile)
-    elif key_mode == "DSS":
+    elif key_mode == "ssh-dss":
         pkey = paramiko.DSSKey.from_private_key(keyfile)
-    elif key_mode == "RSA" or key_mode == "OPENSSH":
+    elif key_mode == "ssh-rsa":
         pkey = paramiko.RSAKey.from_private_key(keyfile)
+    else:
+        raise ValueError(f"could not recognize {key_mode} as a key type.")
     return Connection(host=server.host, user=server.username, port=server.sshport, connect_kwargs={"pkey": pkey})
 
 def copy_folder(conn: Connection, local: str, remote: str):
