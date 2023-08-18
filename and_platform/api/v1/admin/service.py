4a3adaf7a9e28d62cce17d4d00c20524eb455a27
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2023-08-18 22:02:28 +0700

    fix error handling

--- and_platform/api/v1/admin/service.py
+++ and_platform/api/v1/admin/service.py
@@ -1,8 +1,7 @@
-from and_platform.models import db, Challenges, Teams, Services, ScorePerTicks
+from and_platform.models import db, Challenges, Teams, Services
 from and_platform.core.config import get_config
 from and_platform.core.service import do_provision
-from flask import Blueprint, jsonify, request, views
-from itertools import product
+from flask import Blueprint, jsonify, request, views, current_app as app
 
 service_blueprint = Blueprint("service", __name__, url_prefix="/services")
 
@@ -27,20 +26,23 @@ class ServiceProvision(views.MethodView):
             or (isinstance(provision_challs, list) and len(challenges) != len(provision_challs)):
             return jsonify(status="failed", message="challenge or team cannot be found."), 400
         
-        try:
-            server_mode = get_config("SERVER_MODE")
-            for team in teams:
-                for chall in challenges:
-                    if Services.is_teamservice_exist(team.id, chall.id): continue
-
-                    if server_mode == "private": server = team.server
-                    else: server = chall.server
-
+        server_mode = get_config("SERVER_MODE")
+        for team in teams:
+            for chall in challenges:
+                if Services.is_teamservice_exist(team.id, chall.id): continue
+                if server_mode == "private": server = team.server
+                else: server = chall.server
+                
+                try:
                     services = do_provision(team, chall, server)
-                    db.session.add_all(services)
-            db.session.commit()
-        except Exception as e:
-            return jsonify(status="failed", message=e), 500
+                except Exception as ex:
+                    error_msg = f"error when provisioning challenge id={chall.id} for team id={team.id}: {ex}"
+                    app.logger.error(ex, exc_info=True)
+                    return jsonify(status="failed", message=error_msg), 500
+
+                db.session.add_all(services)
+                db.session.commit()
+        
         return jsonify(status="success", message="successfully provision all requested services.")
 
 service_blueprint.add_url_rule('/provision', view_func=ServiceProvision.as_view('service_provision'))
