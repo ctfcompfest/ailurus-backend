644ece001c785b3137f25c6ae2d452ab3bab8c7b
Rhfialyndra <l>
2023-08-19 08:45:08 +0700

    implements public/teams

--- and_platform/api/v1/__init__.py
+++ and_platform/api/v1/__init__.py
@@ -2,8 +2,10 @@ from flask import Blueprint
 from and_platform.api.v1.admin import adminapi_blueprint
 from and_platform.api.v1.submission import submission_blueprint
 from and_platform.api.v1.contest import public_contest_blueprint
+from and_platform.api.v1.teams import public_teams_blueprint
 
 apiv1_blueprint = Blueprint("apiv1", __name__, url_prefix="/v1")
 apiv1_blueprint.register_blueprint(adminapi_blueprint)
 apiv1_blueprint.register_blueprint(submission_blueprint)
 apiv1_blueprint.register_blueprint(public_contest_blueprint)
+apiv1_blueprint.register_blueprint(public_teams_blueprint)
