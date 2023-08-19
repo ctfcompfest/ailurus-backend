32dfcb2e7b85a3046f4206e7f576b73bed2a207b
Rendy Arya Kemal <renrror@gmail.com>
2023-08-19 22:40:07 +0700

    Check by password hash

--- and_platform/api/v1/authenticate.py
+++ and_platform/api/v1/authenticate.py
@@ -2,11 +2,14 @@ from and_platform.core.config import get_config
 from flask import Blueprint, jsonify, request
 from sqlalchemy import select
 from flask_jwt_extended import create_access_token
-import jwt
+from werkzeug.security import check_password_hash
 
 from and_platform.models import db, Teams
 
-authenticate_blueprint = Blueprint("authenticate_blueprint", __name__, url_prefix="/authenticate")
+authenticate_blueprint = Blueprint(
+    "authenticate_blueprint", __name__, url_prefix="/authenticate"
+)
+
 
 @authenticate_blueprint.route("/", methods=["POST"])
 def login():
@@ -14,11 +17,13 @@ def login():
     password = request.json.get("password")
 
     team = db.session.execute(
-        select(Teams).filter(Teams.email == email).filter(Teams.password == password)
+        select(Teams).filter(Teams.email == email)
     ).scalar_one_or_none()
-    
-    if team is not None:
-        access_token = create_access_token(identity={'team': {'id': team.id, 'name': team.name}})
-        return jsonify(status='success', data=access_token), 200
 
-    return jsonify(status='forbidden', message="email or password is wrong."), 403
+    if team is not None and check_password_hash(team.password, password):
+        access_token = create_access_token(
+            identity={"team": {"id": team.id, "name": team.name}}
+        )
+        return jsonify(status="success", data=access_token), 200
+
+    return jsonify(status="forbidden", message="email or password is wrong."), 403
