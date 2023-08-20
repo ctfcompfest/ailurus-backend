2506c418f278beab5d22bafe4e77881213952a1b
Rhfialyndra <l>
2023-08-20 22:12:33 +0700

    implement public/challenges

---
+++ and_platform/api/v1/challenge.py
@@ -0,0 +1,54 @@
+from and_platform.models import db, Challenges, ChallengeReleases
+from and_platform.api.helper import convert_model_to_dict;
+from flask import Blueprint, jsonify, request;
+
+from sqlalchemy import select, Row;
+
+public_challenge_blueprint = Blueprint("public_challenge_blueprint", __name__, url_prefix="/challenges")
+
+
+@public_challenge_blueprint.route("/", methods=["GET"])
+def get_all_challenge():
+    
+    visible_challenges = [] 
+    visible_challenges_id = get_all_released_challenge_id()   
+    challenges = Challenges.query.with_entities(Challenges.id, Challenges.name, Challenges.description).filter(Challenges.id.in_(visible_challenges_id)).all()
+
+    for challenge in challenges:
+        data = {
+            "id" : challenge[0],
+            "name" : challenge[1],
+            "descripttion" :challenge[2]
+        }
+        visible_challenges.append(data)
+
+    print(visible_challenges)
+
+    return jsonify(status="success", data=visible_challenges), 200
+
+@public_challenge_blueprint.route("/<int:challenge_id>", methods=["GET"])
+def get_challenge_by_id(challenge_id):
+    visible_challenges_id = get_all_released_challenge_id()
+
+    if challenge_id not in visible_challenges_id:
+        return jsonify(status="failed", message="challenge not found"), 404
+
+    challenge = Challenges.query.with_entities(Challenges.id, Challenges.name, Challenges.description).filter_by(id=challenge_id).first()
+    data = {
+            "id" : challenge[0],
+            "name" : challenge[1],
+            "descripttion" :challenge[2]
+        }
+    return jsonify(status="success", data=data), 200
+
+def get_all_released_challenge_id() -> list:
+    visible_challenges_id = []
+    challenge_releases : Row[ChallengeReleases] = ChallengeReleases.query.all()
+
+    if not isinstance(challenge_releases, list):
+        challenge_releases = [challenge_releases]
+    challenge_releases = convert_model_to_dict(challenge_releases)
+
+    for release in challenge_releases:
+        visible_challenges_id.append(release.get("challenge_id", 0))
+    return visible_challenges_id
