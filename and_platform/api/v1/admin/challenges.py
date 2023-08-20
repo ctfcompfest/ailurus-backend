659017d776bf1cb1012f6058ea403d9689a6b7b3
Rendy Arya Kemal <renrror@gmail.com>
2023-08-20 18:04:02 +0700

    Set visibility on update/create

--- and_platform/api/v1/admin/challenges.py
+++ and_platform/api/v1/admin/challenges.py
@@ -1,4 +1,5 @@
-from sqlalchemy import select
+from typing import List
+from sqlalchemy import delete, insert, select
 from and_platform.api.helper import convert_model_to_dict
 from and_platform.core.challenge import (
     ChallengeData,
@@ -6,10 +7,14 @@ from and_platform.core.challenge import (
     load_challenge,
 )
 from and_platform.core.config import get_config
-from and_platform.models import Challenges, Servers, db
+from and_platform.models import ChallengeReleases, Challenges, Servers, db
 from flask import Blueprint, jsonify, request
 
 challenges_blueprint = Blueprint("challenges", __name__, url_prefix="/challenges")
+
+
+class ChallengeRequest(ChallengeData):
+    visibility: List[int]
 
 
 @challenges_blueprint.post("/populate")
@@ -66,14 +71,15 @@ def get_all_challs():
 @challenges_blueprint.post("/")
 def create_new_chall():
     server_mode = get_config("SERVER_MODE")
-    data: ChallengeData = request.get_json()
+    data: ChallengeRequest = request.get_json()
 
     name = data.get("name")
     description = data.get("description")
     num_expose = data.get("num_expose")
     server_id = data.get("server_id")
+    visibility = data.get("visibility")
 
-    if not (name and description and num_expose) or (
+    if not (name and description and num_expose and visibility) or (
         server_mode == "sharing" and not server_id
     ):
         return jsonify(status="failed", message="missing required attributes"), 400
@@ -93,6 +99,8 @@ def create_new_chall():
 
     db.sesion.add(chall)
     db.session.commit()
+
+    set_chall_visibility(chall.id, visibility)
     return jsonify(status="success", data=convert_model_to_dict(chall))
 
 
@@ -108,7 +116,7 @@ def get_chall(challenge_id: int):
 @challenges_blueprint.post("/<int:challenge_id>")
 def update_chall(challenge_id: int):
     server_mode = get_config("SERVER_MODE")
-    data: ChallengeData = request.get_json()
+    data: ChallengeRequest = request.get_json()
 
     chall = db.session.get(Challenges, challenge_id)
     if not chall:
@@ -118,6 +126,17 @@ def update_chall(challenge_id: int):
     chall.description = data.get("description", chall.description)
     chall.num_expose = data.get("num_expose", chall.num_expose)
     server_id = data.get("server_id", chall.server_id)
+    visibility = data.get("visibility")
+
+    # It might be [], in which we'll completely hide it, this case is to use
+    # db's default
+    if visibility == None:
+        releases = db.session.execute(
+            select(ChallengeReleases).filter(
+                ChallengeReleases.challenge_id == challenge_id
+            )
+        ).scalars()
+        visibility: List[int] = [r.round for r in releases]  # type: ignore
 
     if server_mode == "sharing":
         server = db.session.execute(
@@ -127,6 +146,7 @@ def update_chall(challenge_id: int):
         chall.server_host = server.host
 
     db.session.commit()
+    set_chall_visibility(challenge_id, visibility)
     return jsonify(status="success", data=convert_model_to_dict(chall))
 
 
@@ -139,3 +159,16 @@ def delete_chall(challenge_id: int):
     db.session.delete(chall)
     db.session.commit()
     return jsonify(status="success", message=f"challenge {challenge_id} deleted")
+
+
+def set_chall_visibility(chall_id: int, rounds: List[int]):
+    # Delete all releases, then re-insert is the easiest way to handle update
+    db.session.execute(
+        delete(ChallengeReleases).filter(ChallengeReleases.challenge_id == chall_id)
+    )
+
+    statement = insert(ChallengeReleases)
+    for round in rounds:
+        statement = statement.values(round=round, challenge_id=chall_id)
+
+    db.session.execute(statement)
