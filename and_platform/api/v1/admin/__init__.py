c974f4be795c38d91556fe4ddcb05223c4d9ebb3
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2023-08-21 21:40:23 +0700

    Fix integrity error when populate with non empty db (#43)
    * Fix integrity error when populate with non empty db
    
    * renaming challenge yaml file

--- and_platform/api/v1/admin/__init__.py
+++ and_platform/api/v1/admin/__init__.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from and_platform.api.v1.admin.challenges import challenges_blueprint
 from and_platform.api.v1.admin.contest import contest_blueprint
 from and_platform.api.v1.admin.service import service_blueprint
 from and_platform.api.v1.admin.servers import servers_blueprint
@@ -6,6 +7,7 @@ from and_platform.core.security import admin_only
 
 adminapi_blueprint = Blueprint("admin", __name__, url_prefix="/admin")
 adminapi_blueprint.before_request(admin_only)
+adminapi_blueprint.register_blueprint(challenges_blueprint)
 adminapi_blueprint.register_blueprint(contest_blueprint)
 adminapi_blueprint.register_blueprint(service_blueprint)
 adminapi_blueprint.register_blueprint(servers_blueprint)
