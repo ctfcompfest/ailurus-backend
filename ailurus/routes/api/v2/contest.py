9f85c1ab876ab7f17ec6d9fe7ae9168c9aa532ad
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2024-07-25 21:30:26 +0700

    Implement apiv2/contestinfo

---
+++ ailurus/routes/api/v2/contest.py
@@ -0,0 +1,26 @@
+from ailurus.utils.config import get_config, is_contest_running, is_contest_finished, is_contest_paused
+from flask import Blueprint, jsonify
+
+contestinfo_blueprint = Blueprint("contestinfo", __name__, url_prefix="/contest")
+
+@contestinfo_blueprint.get("/info")
+def get_contest_info():
+    event_status = "not started"
+    if is_contest_running():
+        event_status = "running"
+    elif is_contest_finished():
+        event_status = "finished"
+    elif is_contest_paused(): 
+        event_status = "paused"
+    
+    response_data = {
+        "event_name": get_config("EVENT_NAME"),
+        "logo_url": get_config("LOGO_URL"),
+        "start_time": get_config("START_TIME").isoformat(),
+        "number_round": get_config("NUMBER_ROUND"),
+        "number_tick": get_config("NUMBER_TICK"),
+        "current_round": get_config("CURRENT_ROUND", 0),
+        "current_tick": get_config("CURRENT_TICK", 0),
+        "event_status": event_status,
+    }
+    return jsonify(status="success", data=response_data)
