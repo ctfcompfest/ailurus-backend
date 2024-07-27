bc3437da32f054957647917bd1941fac0f9c2b05
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2024-07-27 14:17:33 +0700

    Chore: delete unused things

--- and_platform/api/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from and_platform.api.v1 import apiv1_blueprint
-
-api_blueprint = Blueprint("api", __name__, url_prefix="/api")
-api_blueprint.register_blueprint(apiv1_blueprint)
-
-@api_blueprint.get('/ping')
-def ping():
-    return 'pong'
--- and_platform/core/constant.py
+++
@@ -1,4 +0,0 @@
-from datetime import timedelta
-
-CHECKER_INTERVAL = timedelta(seconds=60)
-CHECKER_TIMEOUT = timedelta(seconds=CHECKER_INTERVAL.total_seconds() // 3)
