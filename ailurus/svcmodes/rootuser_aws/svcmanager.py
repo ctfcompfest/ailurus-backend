7e96d945b759a29cfc27b231a99dd0ce42dbecb4
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2024-07-31 21:46:42 +0700

    Reformat init_artifact

--- ailurus/svcmodes/rootuser_aws/svcmanager.py
+++ ailurus/svcmodes/rootuser_aws/svcmanager.py
@@ -9,13 +9,13 @@ import base64
 def init_artifact(body, **kwargs):
     chall_id = body["challenge_id"]
     artifactroot_folder = os.path.join(get_app_config("DATA_DIR"), "..", "worker_data", "artifacts")
-    os.makedirs(artifactroot_folder, exist_ok=True)
-
+    artifact_folder = os.path.join(artifact_folder, f"artifact-{chall_id}")
     artifact_zipfile = os.path.join(artifactroot_folder, f"artifact-{chall_id}.zip")
+    
+    os.makedirs(artifactroot_folder, exist_ok=True)
     with open(artifact_zipfile, 'wb') as fp:
         fp.write(base64.b64decode(body["artifact"]))
     with zipfile.ZipFile(artifact_zipfile, "r") as fp:
-        artifact_folder = os.path.join(artifact_folder, f"artifact-{chall_id}")
         fp.extractall(artifact_folder)
 
 def do_provision(body: ServiceManagerTask, **kwargs):
