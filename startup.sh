cd9242eeac147185e01d0de22c2fb59142620eca
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2024-08-04 21:16:21 +0700

    Fix module required not found for svcmodes

--- startup.sh
+++ startup.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
+# Iterate through each folder in ailurus/svcmodes
+for folder in /opt/app/ailurus/svcmodes/*/; do
+    # Check if requirements.txt file exists in the current folder
+    if [ -f "$folder/requirements.txt" ]; then
+        pip3 install -r "$folder/requirements.txt"
+    fi
+done
+
 CMD=${@:-webapp}
 echo $CMD
 cd /opt/app && \
