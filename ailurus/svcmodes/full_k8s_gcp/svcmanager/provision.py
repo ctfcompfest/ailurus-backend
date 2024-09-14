9919e3fe98dc45a0a96f73ff754ecb275dcbbe87
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2024-09-15 01:19:10 +0700

    Add nodeSelector to minimize number of nodes

--- ailurus/svcmodes/full_k8s_gcp/svcmanager/provision.py
+++ ailurus/svcmodes/full_k8s_gcp/svcmanager/provision.py
@@ -252,7 +252,10 @@ def create_service_deployment(
                                 {"mountPath": "/root/.ssh", "name": "volpvc", "subPath": deployment_name+"/ssh", "readOnly": True},
                             ]
                         }
-                    ]
+                    ],
+                    "nodeSelector": {
+                        "cloud.google.com/gke-cpu-scaling-level": "8"
+                    }
                 }
             }
         },
