04180a5b26492e830f71489b4936048111a74b4a
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2025-08-10 03:20:43 +0700

    fix: socket gevent

--- wsgi.py
+++ wsgi.py
@@ -7,5 +7,5 @@ webapp = create_webapp_daemon()
 with webapp.app_context():
     flask_migrate.upgrade()
     
-sio = socketio.Server()
+sio = socketio.Server(cors_allowed_origin="*", engineio_logger=True)
 app = socketio.WSGIApp(sio, webapp)
