14e32bca7e946b750b6d1ad34ce60bcaad109cdb
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2025-08-10 03:44:07 +0700

    fix: socket

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,6 @@
 from gevent import monkey
 
 from ailurus import create_webapp_daemon
-from ailurus.utils.socket import socketio
 
 import socketio
 import flask_migrate
@@ -10,5 +9,8 @@ monkey.patch_all()
 
 webapp = create_webapp_daemon()
 with webapp.app_context():
+    from ailurus.utils.socket import socketio
+    
     flask_migrate.upgrade()
+    
     app = socketio.WSGIApp(socketio, webapp)
