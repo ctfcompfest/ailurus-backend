56c2a199f2efbf5f7098f0b65fa946b5678dff70
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2025-08-10 03:47:54 +0700

    debug: socket

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,9 @@
-from gevent import monkey
-
 from ailurus import create_webapp_daemon
 
-import socketio
 import flask_migrate
-
+from gevent import monkey
 monkey.patch_all()
 
-webapp = create_webapp_daemon()
-with webapp.app_context():
-    from ailurus.utils.socket import socketio
-    
+app = create_webapp_daemon()
+with app.app_context():
     flask_migrate.upgrade()
-    
-    app = socketio.WSGIApp(socketio, webapp)
