fa0e0b8534247420e6c874f02e8a4eb73f7050e1
Muhammad Faishol Amirul Mukminin <faishol.amirul27@gmail.com>
2025-08-09 13:06:05 +0700

    fix: duplicate key in migrations svcmode

--- ailurus/svcmodes/lksn/migrations/1c0af4ba48be_add_checkeragent_table copy.py
+++ ailurus/svcmodes/lksn/migrations/1c0af4ba48be_add_checkeragent_table copy.py
@@ -31,12 +31,15 @@ def upgrade(op):
         sa.column("key", sa.String()),
         sa.column("value", sa.String())
     )
-    op.bulk_insert(config_table, [
-        {
-            "key": "CHECKER_AGENT_SECRET",
-            "value": "secret"
-        },
-    ])
+    try:
+        op.bulk_insert(config_table, [
+            {
+                "key": "CHECKER_AGENT_SECRET",
+                "value": "secret"
+            },
+        ])
+    except Exception:
+        pass
 
 
 def downgrade(op):
